68448eae171c8e23a4265d09294b2c09cff14cc1
emm-ser <106416@bk.ru>
2018-02-27 23:55:21 +0300

    quadratic-equation/feature: solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  
+    equation = equation.replace(/ /g, "");
+    a = +equation.substr(0, equation.indexOf("*x^2"));
+    b = +equation.substr(equation.indexOf("*x^2") + 4, equation.lastIndexOf("*x") - equation.indexOf("*x^2") - 4);
+    c = +equation.substr(equation.lastIndexOf("*x") + 2);
+
+    d = b * b - 4 * a * c;
+    x1 = Math.round(( -b + Math.sqrt(d) ) / ( 2 * a ));
+    x2 = Math.round(( -b - Math.sqrt(d) ) / ( 2 * a ));
+
+    console.log(a);
+    console.log(b);
+    console.log(c);
+
+    return x1 > x2 ? [x2, x1] : [x1, x2];
 }
